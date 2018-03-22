3a09cefe1b971fdebb6a454fce738c97fa6f008a
Patty Kovash <pattykovash@gmail.com>
2018-03-22 10:49:49 -0600

    Add styling

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -5,19 +5,19 @@ document.addEventListener('DOMContentLoaded', (e) => {
   function setHour() {
     const curHour = (new Date()).getHours() % 12;
     const hourDeg = 90 + (30 * curHour)
-    hour.style.transform = `rotate(${hourDeg}deg)`;
+    hour.style.transform = `rotate(${hourDeg}deg) translateY(-2px)`;
   }
 
   function setMin() {
     const curMin = (new Date()).getMinutes();
     const minDeg = ((curMin / 60) * 360) + 90;
-    min.style.transform = `rotate(${minDeg}deg)`;
+    min.style.transform = `rotate(${minDeg}deg) translateY(-2px)`;
   }
 
  function setSec() {
     const curSec = (new Date()).getSeconds();
     const secDeg = ((curSec / 60) * 360) + 90;
-    sec.style.transform = `rotate(${secDeg}deg)`;
+    sec.style.transform = `rotate(${secDeg}deg) translateY(-1.5px)`;
   }
 
   function setHands(time, func) {
@@ -30,5 +30,4 @@ document.addEventListener('DOMContentLoaded', (e) => {
   setHands(1000 * 60 * 60, setHour);
   setHands(1000 * 60, setMin);
   setHands(1000, setSec);
-
 });
